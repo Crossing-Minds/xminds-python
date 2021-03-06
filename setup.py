d322438ec4a1e46f4fbc9ba36a2432212cfdb9c9
Anne Spitz <anne.spitz@crossingminds.com>
2021-03-06 06:29:40 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+""" Crossing Minds data science python library and API client
+
+See our [readthedocs](https://xminds.readthedocs.io/en/latest/) for the Python Data Science library documentation.
+See the [API Documentation](https://docs.api.crossingminds.com/) for the Crossing Minds universal recommendation API documentation.
+
+Our tools are licensed under the MIT License.
+"""
+DOCLINES = (__doc__ or '').split("\n")
+
 import setuptools
 
 from xminds import __version__
-
-
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name='xminds',
     version=__version__,
     author='Crossing Minds, Inc',
     author_email='contact@crossingminds.com',
-    description='Crossing Minds data science python library and API client',
-    long_description=long_description,
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
     long_description_content_type='text/markdown',
     url='https://github.com/Crossing-Minds/xminds-python',
     packages=['xminds', 'xminds.api', 'xminds.lib', 'xminds.ds', 'xminds._lib'],
